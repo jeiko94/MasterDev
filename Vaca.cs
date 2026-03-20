0cf503bd1e15ab810d31353706dcc2564d42b7d7
Diego Muñoz <alejo31mzz@gmail.com>
2026-03-19 21:11:36 -0500

    Reto 5: animales

---
+++ Vaca.cs
@@ -0,0 +1,12 @@
+﻿namespace MasterDev
+{
+    public class Vaca : ISonido
+    {
+        public async Task<string> HacerSonido(string nombreSonido)
+        {
+            var sonido = string.IsNullOrWhiteSpace(nombreSonido) ? "muuuu" : nombreSonido;
+
+            return await Task.FromResult($"La vaca hace {sonido}!");
+        }
+    }
+}
