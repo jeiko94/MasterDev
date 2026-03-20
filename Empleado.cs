dd513a804299618b28d0ff7291f6d90d1f4fbeb5
Mario Alejandro Peña arenas <marioalejo_p@hotmail.com>
2026-03-19 21:12:28 -0500

    reto 4

---
+++ Empleado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Empleado
+{
+    public string Nombre { get; set; }
+
+    public Empleado(string nombre)
+    {
+        Nombre = nombre;
+    }
+
+    public virtual double CalcularSalario()
+    {
+        return 0;
+    }
+}
